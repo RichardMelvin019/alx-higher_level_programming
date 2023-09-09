c84a135939497222c8388b47372087992e5f30c0
RichardMelvin019 <richardmelvin019@gmail.com>
2023-09-09 10:40:02 +0100

    undone

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+	import sys
+
+	argc = len(sys.argv) - 1
+	argv = sys.argv[1:]
+
+	for i, arguments in enumerate(argv, 1):
+		if argc == 1:
+			print("{:d} argument:".format(argv))
