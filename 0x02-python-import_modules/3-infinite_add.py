8ee1a5d98336380f9215ac30cb6f1ee255772e15
RichardMelvin019 <richardmelvin019@gmail.com>
2023-09-10 18:04:03 -0700

    import & modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Prints the sum of the argv"""
+    import sys
+
+    argv = sys.argv[1:]
+    result = 0
+
+    for i, arguments in enumerate(argv, 1):
+        result += int(arguments)
+    print("{}".format(result))
