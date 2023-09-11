a598cc8c3de279490f72fab5101b5b59c6173f41
RichardMelvin019 <richardmelvin019@gmail.com>
2023-09-10 17:06:14 -0700

    updated

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	"""Prints the sum of the argv"""
-	import sys
+    """Prints the sum of the argv"""
+    import sys
 
-	argv = sys.argv[1:]
-	result = 0
+    argv = sys.argv[1:]
+    result = 0
 
-	for i, arguments in enumerate(argv, 1):
-		result += int(arguments)
-	print("{}".format(result))
+    for i, arguments in enumerate(argv, 1):
+	    result += int(arguments)
+    print("{}".format(result))
