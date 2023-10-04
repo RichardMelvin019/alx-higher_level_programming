bfeda5b1435373a4582ae48415f1082210b0a600
RichardMelvin019 <richardmelvin019@gmail.com>
2023-10-03 21:39:11 -0700

    Max integer - Unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
-"""Unittests for max_integer([..])."""
+"""Unittests for max_integer. """
 
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Define unittests for max_integer([..])."""
+    """Define unittests for max_integer. """
 
-    def test_ordered_list(self):
+    def test_arranged_list(self):
         """Test an ordered list of integers."""
-        ordered = [1, 2, 3, 4]
-        self.assertEqual(max_integer(ordered), 4)
+        arranged = [1, 2, 3, 4]
+        self.assertEqual(max_integer(arranged), 4)
 
-    def test_unordered_list(self):
+    def test_unarranged_list(self):
         """Test an unordered list of integers."""
-        unordered = [1, 2, 4, 3]
-        self.assertEqual(max_integer(unordered), 4)
+        unarranged = [1, 2, 4, 3]
+        self.assertEqual(max_integer(unarranged), 4)
 
-    def test_max_at_begginning(self):
+    def test_max_at_start(self):
         """Test a list with a beginning max value."""
-        max_at_beginning = [4, 3, 2, 1]
-        self.assertEqual(max_integer(max_at_beginning), 4)
+        max_at_start = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_start), 4)
 
     def test_empty_list(self):
         """Test an empty list."""
         empty = []
         self.assertEqual(max_integer(empty), None)
 
-    def test_one_element_list(self):
+    def test_single_element_list(self):
         """Test a list with a single element."""
-        one_element = [7]
-        self.assertEqual(max_integer(one_element), 7)
+        single_element = [7]
+        self.assertEqual(max_integer(single_element), 7)
 
     def test_floats(self):
         """Test a list of floats."""
